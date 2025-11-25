7baf714a6b2ea524ce3e91a7544170c9f288594a
Dianii-1 <dianii230416@gmail.com>
2025-11-25 18:04:43 -0500

    feat: Add computed bears to obtain all bears

--- src/stores/bears/bear.store.ts
+++ src/stores/bears/bear.store.ts
@@ -10,17 +10,34 @@ interface BearStore {
   pandaBears: number;
 
   bears: Bear[];
+  computed: {
+    totalBears: number;
+  };
 
   increaseBlackBears: (by: number) => void;
   increasePolarBears: (by: number) => void;
   increasePandaBears: (by: number) => void;
+
+  addBear: () => void;
+  cleanBears: () => void;
 }
-export const useBearStore = create<BearStore>()((set) => ({
+export const useBearStore = create<BearStore>()((set, get) => ({
   blackBears: 10,
   pandaBears: 10,
   polarBears: 10,
 
   bears: [{ id: 1, name: "Oso #1" }],
+
+  computed: {
+    get totalBears() {
+      return (
+        get().blackBears +
+        get().pandaBears +
+        get().polarBears +
+        get().bears.length
+      );
+    },
+  },
 
   increaseBlackBears: (by: number) =>
     set((state) => ({ blackBears: state.blackBears + by })),
@@ -28,4 +45,12 @@ export const useBearStore = create<BearStore>()((set) => ({
     set((state) => ({ polarBears: state.polarBears + by })),
   increasePandaBears: (by: number) =>
     set((state) => ({ pandaBears: state.pandaBears + by })),
+  addBear: () =>
+    set((state) => ({
+      bears: [
+        ...state.bears,
+        { id: state.bears.length + 1, name: `Oso #${state.bears.length + 1}` },
+      ],
+    })),
+  cleanBears: () => set({ bears: [] }),
 }));
